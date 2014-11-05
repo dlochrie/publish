ac42b18d6f9f23cf764a0533971881be442295d0
Daniel Lochrie <dclochri@gmail.com>
2014-11-05 13:24:31 -0600

    Added a new Admin portal page, and tests for the admin controller.

---
+++ app/controllers/admin.js
@@ -0,0 +1,24 @@
+/**
+ * Expose the "Administration" Controller.
+ */
+module.exports = new AdministrationController;
+
+
+
+/**
+ * Constructor for the AdministrationController.
+ * @constructor
+ */
+function AdministrationController() {}
+
+
+/**
+ * Renders administration panel.
+ * @param {http.IncomingMessage} req Node/Express request object.
+ * @param {http.ServerResponse} res Node/Express response object.
+ */
+AdministrationController.prototype.index = function(req, res, next) {
+  res.render('admin/index', {
+    title: 'Admin Panel'
+  });
+};
---
+++ test/controllers/admin_test.js
@@ -0,0 +1,21 @@
+var request = require('supertest');
+
+
+describe('admin controller test', function() {
+  it('should show the admin page', function(done) {
+    request(app).
+        get('/admin').
+        expect('Content-Type', /html/).
+        expect(200).
+        end(function(err, res) {
+          if (res.text) {
+            // Assert that all the panel headings are available.
+            res.text.should.match(/Manage Posts/);
+            res.text.should.match(/Manage Users/);
+            res.text.should.match(/Manage Categories/);
+            res.text.should.match(/Manage Features/);
+          }
+          done(err);
+        });
+  });
+});
