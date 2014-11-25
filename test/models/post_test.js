2a0c41e0bd676322edd426b2ad7fc42db7cd9115
Daniel Lochrie <dclochri@gmail.com>
2014-11-24 20:28:32 -0600

    Added the rest of the Post model tests.

--- test/models/post_test.js
+++ test/models/post_test.js
@@ -1,7 +1,38 @@
+Post = require('../../app/models/post');
+
 describe('post model test', function() {
-  it('should inherit from the base model class', function(done) { done(); });
-  it('should provide default values', function(done) { done(); });
-  it('should provide a model associations object', function(done) { done(); });
-  it('should provide a list of select columns', function(done) { done(); });
-  it('should provide a model structure', function(done) { done(); });
+  var post;
+
+  beforeEach(function(done) {
+    post = new Post(app);
+    done();
+  });
+
+  it('should inherit from the base model class', function() {
+    post.should.be.an.Object.and.have.properties(
+        ['find', 'findOne', 'select', 'getColumns']);
+  });
+
+  it('should provide default values', function() {
+    post.modelAssociations.should.equal(Post.MODEL_ASSOCIATIONS_);
+    post.tableColumns.should.equal(Post.SELECT_COLUMNS_);
+    post.structure.should.equal(Post.STRUCTURE_);
+  });
+
+  it('should provide a model associations object', function() {
+    post.modelAssociations.should.be.an.Object.and.have.properties(
+        ['user', 'categories']);
+  });
+
+  it('should provide a list of select columns', function() {
+    post.tableColumns.should.be.an.Array;
+    post.tableColumns.indexOf('post.title').should.not.eql(-1);
+    post.tableColumns.indexOf('post.description').should.not.eql(-1);
+    post.tableColumns.indexOf('invalid').should.eql(-1);
+  });
+
+  it('should provide a model structure', function() {
+    post.structure.should.be.an.Object.and.have.properties(
+        ['user_id', 'title', 'slug']);
+  });
 });
